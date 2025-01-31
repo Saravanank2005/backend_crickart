4900918f3a4fb95a5f926f5042c6d4fa4d02ddcb
Saravanank2005 <saravanan.k2023it@sece.ac.in>
2025-01-31 11:43:01 +0530

    first commit

--- models/User.js
+++ models/User.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required'],
+    unique: true // Ensures no duplicate usernames
+  },
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, 'Email is required'],
+    unique: true // Ensures no duplicate emails
   },
   password: {
     type: String,
-    required: true
+    required: [true, 'Password is required']
   },
   role: {
     type: String,
